2823920b3b707660dfa7b7ab4774318bd34e87c7
ARISEN-DEV <91806767+ARISEN-DEV@users.noreply.github.com>
2021-11-27 17:20:16 -0300

    izi
    ahora si anda la wea pero mas inseguro que adolescente otako

--- routes/auth.py
+++ routes/auth.py
@@ -10,7 +10,21 @@ seed = bcrypt.gensalt()
 
 @auth.route("/")
 def index():
+
     return render_template('index.html')
+
+# @auth.route("/index", methods=["POST", "GET"])
+# def obtener_usuarios_por_nombre_clave(email, password):
+#     obtener_usuario_sql = f"""
+#             SELECT id, email 
+#             FROM Usuarios
+#             WHERE email='{email}' and password='{password}' 
+#         """
+#     bd = BaseDeDatos()
+#     return [{"id": registro[0],
+#             "nombre": registro[1],
+#             "rol": registro[2]
+#             } for registro in bd.ejecutar_sql(obtener_usuario_sql)]
 
 @auth.route("/index", methods=["POST", "GET"])
 def login():
@@ -19,17 +33,19 @@ def login():
 
         email = request.form["email"]
         password = request.form["password"]
-        password_encode = password.encode("utf-8")
-        password_encrypted = bcrypt.hashpw(password_encode, seed)
-        usuario = db.query.filter_by(email=email).first()
-        
-        if email and password_encrypted in usuario:
+        # password_encode = password.encode("utf-8")
+        # password_encrypted = bcrypt.hashpw(password_encode, seed)
+        usuario = db.session.query(Usuario).filter_by(email=email).first()
+
+        if email==usuario.email and password==usuario.password:
             session['usuario_id'] = usuario.id
-            flash('Me alegra que estes aqui')
-            return redirect('auth.dashboard')
+            flash("Me alegra que estes aqui")
+            return redirect(url_for('auth.dashboard'))
+        
         else:
             flash('El usuario no existe')
             return redirect(url_for('auth.login'))
+        
     return render_template('index.html')
 
 @auth.route("/registrar", methods=["POST", "GET"])
@@ -40,15 +56,15 @@ def registrar():
         nombre = request.form["nombre"]
         email = request.form["email"]
         password = request.form["password"]
-        password_encode = password.encode("utf-8")
-        password_encrypted = bcrypt.hashpw(password_encode, seed)
-        usuario = db.session.query(Usuario.id).filter_by(email=email).first()
+        # password_encode = password.encode("utf-8")
+        # password_encrypted = bcrypt.hashpw(password_encode, seed)
+        usuario = db.session.query(Usuario).filter_by(email=email).first()
         
-        if email and password_encrypted == usuario:
-            flash("Las credenciales ya estan siendo usadas")
-            return redirect(url_for('auth.login'))
+        # if email==usuario.email and password==usuario.password:
+        #     flash("Las credenciales ya estan siendo usadas")
+        #     return redirect(url_for('auth.login'))
 
-        new_user = Usuario(email, password_encrypted, nombre)
+        new_user = Usuario(email, password, nombre)
         db.session.add(new_user)
         db.session.commit()
 
