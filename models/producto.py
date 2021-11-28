e3ce4416af33d71488c6b408ecb07b8f21260fbf
ARISEN-DEV <91806767+ARISEN-DEV@users.noreply.github.com>
2021-11-27 21:54:43 -0300

    login ok, stock bad

--- models/producto.py
+++ models/producto.py
@@ -9,6 +9,7 @@ class Producto(db.Model):
     precio = db.Column(db.Integer)
     vencimiento = db.Column(db.String(100))
     proveedor = db.Column(db.String(100))
+    
 
     def __init__(self, nombre, cantidad, medida, precio, vencimiento, proveedor):
         self.nombre = nombre
--- routes/auth.py
+++ routes/auth.py
@@ -1,30 +1,16 @@
 from flask import g, Blueprint, render_template, request, redirect, url_for, flash, session
+from werkzeug.security import generate_password_hash, check_password_hash
 from models.usuario import Usuario
 from utils.db import db
-import bcrypt
 
 
 auth = Blueprint("auth", __name__)
-seed = bcrypt.gensalt()
 
 
 @auth.route("/")
 def index():
 
     return render_template('index.html')
-
-# @auth.route("/index", methods=["POST", "GET"])
-# def obtener_usuarios_por_nombre_clave(email, password):
-#     obtener_usuario_sql = f"""
-#             SELECT id, email 
-#             FROM Usuarios
-#             WHERE email='{email}' and password='{password}' 
-#         """
-#     bd = BaseDeDatos()
-#     return [{"id": registro[0],
-#             "nombre": registro[1],
-#             "rol": registro[2]
-#             } for registro in bd.ejecutar_sql(obtener_usuario_sql)]
 
 @auth.route("/index", methods=["POST", "GET"])
 def login():
@@ -33,18 +19,28 @@ def login():
 
         email = request.form["email"]
         password = request.form["password"]
-        # password_encode = password.encode("utf-8")
-        # password_encrypted = bcrypt.hashpw(password_encode, seed)
         usuario = db.session.query(Usuario).filter_by(email=email).first()
-
-        if email==usuario.email and password==usuario.password:
-            session['usuario_id'] = usuario.id
-            flash("Me alegra que estes aqui")
-            return redirect(url_for('auth.dashboard'))
+        if usuario is not None or False:
+            check = check_password_hash(usuario.password, password)
+            log = email==usuario.email
+            if log and check:
+                session['usuario_id'] = usuario.id
+                flash("Me alegra que estes aqui")
+                return redirect(url_for('auth.dashboard'))
+            else:
+                flash('Contraseña incorrecta')
+            return redirect(url_for('auth.login'))    
         
         else:
-            flash('El usuario no existe')
+            flash('Usuario incorrecto')
             return redirect(url_for('auth.login'))
+        # elif email!=usuario.email:
+        #     flash('El usuario no existe')
+        #     return redirect(url_for('auth.login'))
+        
+        # elif check_password_hash(usuario.password, password) == False:
+        #     flash('El usuario no existe')
+        #     return redirect(url_for('auth.login'))
         
     return render_template('index.html')
 
@@ -56,15 +52,14 @@ def registrar():
         nombre = request.form["nombre"]
         email = request.form["email"]
         password = request.form["password"]
-        # password_encode = password.encode("utf-8")
-        # password_encrypted = bcrypt.hashpw(password_encode, seed)
+        password_encrypted = generate_password_hash(password)
         usuario = db.session.query(Usuario).filter_by(email=email).first()
         
         # if email==usuario.email and password==usuario.password:
         #     flash("Las credenciales ya estan siendo usadas")
         #     return redirect(url_for('auth.login'))
 
-        new_user = Usuario(email, password, nombre)
+        new_user = Usuario(email, password_encrypted, nombre)
         db.session.add(new_user)
         db.session.commit()
 
@@ -79,4 +74,16 @@ def logout():
 
 @auth.route('/dashboard')
 def dashboard():
+    if 'usuario_id' in session:
         return render_template('dashboard.html')
+    else:
+        flash('Por favor, inicie sesion para acceder a esta pagina')
+        return redirect(url_for('auth.login'))
+
+@auth.route('/stock')
+def stock():
+    if 'usuario_id' in session:
+        return render_template('stock.html')
+    else:
+        flash('Por favor, inicie sesion para acceder a esta pagina')
+        return redirect(url_for('auth.login'))
